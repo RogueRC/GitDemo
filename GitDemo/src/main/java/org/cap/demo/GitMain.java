662c58c7c7b3f19bbc65719bf6be441794cf2198
ranjacho <ranjacho@DIN73002988.corp.capgemini.com>
2018-11-23 14:41:17 +0530

    First Commit

---
+++ GitDemo/src/main/java/org/cap/demo/GitMain.java
@@ -0,0 +1,19 @@
+package org.cap.demo;
+
+public class GitMain {
+	
+	
+	public static String gitDemo() {
+		return "This is Git Demo";
+	}
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Welcome...!!!");
+		
+		gitDemo();
+
+	}
+
+}
